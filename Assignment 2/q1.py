546483c11af6c0a85a01b09d23b2870446ce54d7
Vlad Pac <vlad.pac@hotmail.com>
2023-10-23 17:38:50 -0400

    assignment 2 q4 and q5

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -6,12 +6,21 @@ fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 def V(x, y):
     return 2*x**2 + 2*x*y + y**2
 
-X = np.arange(-5, 5, 0.1)
-Y = np.arange(-5, 5, 0.1)
+dt = 0.1
+X = np.arange(-5, 5, dt)
+Y = np.arange(-5, 5, dt)
 X, Y = np.meshgrid(X, Y)
 
 Z = V(X, Y)
 
 # Surface plot for V(x, y)
 surf = ax.plot_surface(X, Y, Z)
+plt.show()
+
+# Contour Map
+levels = 20
+fig, ax = plt.subplots()
+contour = ax.contour(X, Y, Z, levels)
+ax.clabel(contour, inline=True)
+ax.set_title('Contour Map')
 plt.show()
